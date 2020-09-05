e7d4ff5c301dd7d7a83aba0683f7991049727c8c
fredWills224 <fredwills4229@gmail.com>
2020-09-05 17:37:59 -0400

    search working

--- server/routes/product.js
+++ server/routes/product.js
@@ -58,6 +58,8 @@ router.post('/getProducts', (req, res) =>{
     let skip = parseInt(req.body.skip);
 
     let findArgs = {};
+    let term = req.body.searchTerm;
+
     console.log(req.body.filters);
 
     for (let key in req.body.filters){
@@ -78,17 +80,35 @@ router.post('/getProducts', (req, res) =>{
 
     console.log(findArgs);
 
-    Product.find(findArgs)
-        .populate('writer')
-        .sort([[sortBy, order]])
-        .skip(skip)
-        .limit(limit)
-        .exec((err, products) =>{
-            if(err) return res.status(400).json({ success: false, err });
-            return res.status(200).json({ success: true, products, postSize: products.length });
-        })
-    ;  
-
+    if(term){
+        
+        Product.find(findArgs)
+            .find({ $text: { $search: term } })
+            .populate('writer')
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) =>{
+                if(err) return res.status(400).json({ success: false, err });
+                return res.status(200).json({ success: true, products, postSize: products.length });
+            })
+        ;
+    
+    }else{
+        
+        Product.find(findArgs)
+            .populate('writer')
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) =>{
+                if(err) return res.status(400).json({ success: false, err });
+                return res.status(200).json({ success: true, products, postSize: products.length });
+            })
+        ;
+    
+    }
+  
 });
 
 module.exports = router;
