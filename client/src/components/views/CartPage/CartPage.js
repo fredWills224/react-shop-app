3bc6e70a99f25da2757fa78b833f454c0f31b9ea
fredWills224 <fredwills4229@gmail.com>
2020-09-07 04:24:04 -0400

    userCardBlock

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCartItems } from '../../../_actions/user_actions';
+import UserCardBlock from './Sections/UserCardBlock';
+import { Result, Empty } from 'antd';
 
 function CartPage(props) {
 
     const dispatch = useDispatch();
+    const [Total, setTotal] = useState(0);
+
     useEffect(() => {
         
         let cartItems = [];
@@ -19,9 +23,56 @@ function CartPage(props) {
 
     }, [props.user.userData])
 
+    useEffect(() => {
+        
+        if(props.user.cartDetail && props.user.cartDetail.length > 0){
+            calculateTotal(props.user.cartDetail);
+        }
+
+    }, [props.user.cartDetail])
+
+    const calculateTotal = (cartDetail) =>{
+        let total = 0;
+
+        cartDetail.map(item =>{
+            total += parseInt(item.price, 10) * item.quantity;
+        });
+
+        setTotal(total);
+    }
+
     return (
         <div>
-            CartPage            
+
+            <h1>My Cart</h1>
+            <div>
+
+                <UserCardBlock
+                    products={props.user.cartDetail}
+                />    
+            
+                <div style={{ marginTop: '3rem' }}>
+                    <h2>Total amount: ${Total} </h2>
+                </div>
+
+                <Result
+                    status='success'
+                    title='Successfully Purchased Items'
+                />
+
+                <div
+                    style={{ width: '100%', display: 'flex', flexDirection: 'column',
+                    justifyContent: 'center' 
+                }}>
+
+                    <br/>
+                    <Empty description={false}/>
+                    <p>No Items In the Cart</p>
+                
+                </div>
+
+            </div>
+
         </div>
     )
 }
