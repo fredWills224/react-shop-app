6aecdc97c56800e77424d3c65cdd2e43f5a96a6a
fredWills224 <fredwills4229@gmail.com>
2020-09-06 21:48:04 -0400

    menu tab +empty CartPage

---
+++ client/src/components/views/CartPage/CartPage.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+
+function CartPage() {
+
+    // useEffect(() => {
+        
+    // }, [])
+
+    return (
+        <div>
+            CartPage            
+        </div>
+    )
+}
+
+export default CartPage;
